610da3b77de98b94c65e2f47b0a863b64994a0ae
Billal-Hossen <mddipu867@gmail.com>
2023-12-19 23:21:01 +0600

    Added env sample file

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.use(express.json())
 app.use(routes)
 
 app.use((err, _req, res, next) => {
-  // TODO: format error
   console.log(err);
   res.status(err.status || 500).json({
     message: err.message,
